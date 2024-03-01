a8051708f84ef30d4689d22f2dc744085e1177ae
Oluwatodimu Adegoke <adegokeoluwatodimu@yahoo.com>
2024-02-29 22:11:40 -0600

    first commit

---
+++ main.cpp
@@ -0,0 +1,124 @@
+#include <iostream>
+#include <raylib.h>
+
+using namespace std;
+class Ball{
+    private:
+    float x, y;
+    int speed_x, speed_y;
+    int radius;
+    public:
+
+    float getY(){
+        return y;
+    }
+    Ball(float x1 , float y1, int speed_x1, int speed_y1, int radius1){
+        x = x1;
+        y = y1;
+        speed_x = speed_x1;
+        speed_y = speed_y1;
+        radius = radius1;
+    }
+    void Draw(){
+        DrawCircle(x, y, radius, YELLOW);
+    }
+    void Update(){
+        x += speed_x;
+        y += speed_y;
+        if(y+radius >= GetScreenHeight() || y - radius <= 0){
+            speed_y *= -1;
+        }
+        if(x+radius >= GetScreenWidth() || x -radius <= 0){
+            speed_x *= -1;
+        }
+
+    }
+};
+
+class Paddle{
+    protected:
+    void LimitMovement(){
+        if(y <= 0){
+            y = y + speed;
+        }
+        if(y + height >= GetScreenHeight()){
+            y = y - speed;
+        }
+    }
+    public:
+    float x,y;
+    float width, height;
+    int speed;
+
+    void Draw(){
+        DrawRectangle(x, y, width, height, WHITE);
+    }
+
+    void Update(){
+        if(IsKeyDown(KEY_UP) ){
+            y = y - speed;
+        }
+        if(IsKeyDown(KEY_DOWN)){
+            y = y + speed;
+        }
+        LimitMovement();
+    }
+};
+
+class CpuPaddle : public Paddle{
+    public:
+    void Update(int ball_y){
+        if(y + height/2 > ball_y){
+            y = y - speed;
+        }
+        if(y + height/2 <= ball_y){
+            y = y + speed;
+        }
+        LimitMovement();
+    }
+};
+
+int main () {
+    cout << "Starting the game" << endl;
+    const int screen_width = 1280, screen_height = 800;
+    InitWindow(screen_width, screen_height, "Pong Game!");
+    SetTargetFPS(60);
+
+    Paddle player;
+    CpuPaddle cpu;
+    Ball ball = Ball(screen_width/2 , screen_height/2, 7, 7, 20);
+
+    player.width = 25;
+    player.height = 120;
+    player.x = screen_width - player.width - 10;
+    player.y = screen_height/2 - player.height/2;
+    player.speed = 6;
+    cpu.width = 25;
+    cpu.height = 120;
+    cpu.x = 10;
+    cpu.y = screen_height/2 - player.height/2;
+    cpu.speed = 6;
+
+
+    while(!WindowShouldClose()){
+        BeginDrawing();
+        
+        //Update the ball's position before drawing
+        ball.Update();
+        player.Update();
+        cpu.Update(ball.getY());
+
+        // Clear the background every loop
+        ClearBackground(BLACK);
+        DrawLine(screen_width/2, 0 , screen_width/2, screen_height, WHITE);
+        ball.Draw();
+
+        cpu.Draw();
+        player.Draw();
+        
+        EndDrawing();
+    }
+
+    CloseWindow();
+    return 0;
+}
